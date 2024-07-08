0674eb07cbb50fea0bb6b546d2d444b7a6aee8c7
Bing <bingzhao@coca-cola.com>
2024-07-08 09:16:52 +0800

    Bella

--- Database/Greenplum/sys.sql
+++ Database/Greenplum/sys.sql
@@ -28,6 +28,28 @@ END $$;
     system table
 ***************************************************************/
 
+-- set role
+CREATE ROLE "tsoft";
+CREATE ROLE "tsoft" LOGIN;
+CREATE ROLE "tsoft" LOGIN PASSWORD 'tsoft';
+GRANT Connect ON DATABASE "db_dalian" TO "market_dfda" WITH GRANT OPTION;
+--表：
+GRANT Usage ON SCHEMA "code" TO "tsoft" WITH GRANT OPTION;
+GRANT Select ON TABLE "code"."t_codetype_manager_test" TO "tsoft" WITH GRANT OPTION;
+GRANT Delete, Insert, References, Select, Trigger, Update ON TABLE "code"."t_dim" TO "tsoft" WITH GRANT OPTION;
+--成员：
+GRANT "tsoft" TO "internet" WITH ADMIN OPTION;
+REVOKE Select ON TABLE "code"."t_dim" FROM "tsoft";
+
+
+
+
+-- terminate processes  
+select * from pg_stat_activity --PID
+select pg_terminate_backend(PID);
+
+SELECT pg_cancel_backend(procpid);
+
 
 -- table or database size
 select pg_size_pretty(pg_relation_size('schema.tablename'));
@@ -84,11 +106,6 @@ and c.relname = 'tablename'
 order by c.relname;
 
 
--- terminate processes  
-select * from pg_stat_activity --PID
-select pg_terminate_backend(PID);
-
-
 
 --change distribution key
 --Random Distribution  (Don't use unique）
@@ -114,6 +131,8 @@ where a.relkind='r' and b.attnum=any(c.attrnums) and a.relname not like '%prt%'
 group by table_name
 order by table_name desc;
 
+
+
 --check distribution key（GP6）
 select 
     d.nspname||'.'||a.relname as table_name,
@@ -125,3 +144,52 @@ inner join pg_catalog.pg_namespace d on a.relnamespace=d.oid
 where a.relkind='r' and b.attnum::text=c.distkey::text and a.relname like 't_tag_ent'
 group by table_name,b.attname
 order by table_name desc;
+
+
+-- table rowcount 
+select 
+    c.nspname as "schema",
+    b.relname as "table",
+    a.description as "description",
+    b.reltuples as "rowcount"
+from pg_class b LEFT JOIN pg_namespace c on b.relnamespace = c.oid
+LEFT JOIN  (select * 
+            from pg_description t
+            where t.objsubid = 0
+            )a on b.oid =a.objoid
+where b.relkind = 'r'
+order by c.nspname,b.relname;
+
+
+-- table info
+select 
+    n.nspname as c_schema,
+    c.relname as c_table,
+    a.attname as c_column,
+    col_description(a.attrelid,a.attnum) as c_column_comment,
+    format_type(a.atttypid,a.atttypmod) as c_column_type
+from pg_class c ,pg_attribute  a ,pg_namespace n 
+where  c.relnamespace = n.oid 
+    and  n.nspname = 'schemaname'
+    and a.attrelid = c.oid
+    and a.attnum >0
+    and format_type(a.atttypid,a.atttypmod)!='-'
+order by c.relname,a.attnum
+
+
+-- all table in schema
+SELECT 
+    c.relname as tablename,
+    a.attname as name,
+    col_description(a.attrelid,a.attnum) as comment,
+    format_type(a.atttypid,
+    a.atttypmod) as type,
+    a.attnotnull as notnull   
+FROM pg_class as c,pg_attribute as a ,pg_namespace b
+where b.nspname='schemaname'
+    and b.oid = c.relnamespace
+    and a.attrelid = c.oid 
+    and a.attnum>0 
+    --and c.relname like 't_%'
+
+
